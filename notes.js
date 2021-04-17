7d1df87ad813e86934b17b889df3310dab793f5a
JCDMeira <jean.meira10@hotmail.com>
2021-04-17 11:22:05 -0300

    Adiciona arquivo de notas das aulas

---
+++ notes.js
@@ -0,0 +1,23 @@
+//* Notas importantes e links de referência
+
+//| Links
+
+//_ Aula 1
+    //#obs# Código da aula
+//- https://github.com/luizomf/landing-page-cursojs/commit/185c0bc9daa4fcf1cb213ea70ffe4d722c326e28
+
+//_ Aula 2
+    //#obs# Código da aula
+//- https://github.com/luizomf/landing-page-cursojs/commit/31010d033c30e279ef35f9e866f092716b0592aa
+    //#obs# Validador de HTML
+//- https://validator.w3.org/nu/#textarea
+    //#obs# Accessinility check
+//- https://achecker.ca/checker/index.php
+
+//_
+    //#obs# 
+//-
+
+//_
+    //#obs# 
+//-
