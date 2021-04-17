e462c3da7c5ae830f56e149dedc79290cb0ffdb5
JCDMeira <jean.meira10@hotmail.com>
2021-04-17 15:20:49 -0300

    Atualiza notes.js

--- notes.js
+++ notes.js
@@ -2,11 +2,19 @@
 
 //| Links
 
-//_ Aula 1
+//_ Aula 596
     //#obs# Código da aula
 //- https://github.com/luizomf/landing-page-cursojs/commit/185c0bc9daa4fcf1cb213ea70ffe4d722c326e28
 
-//_ Aula 2
+//_ Aula 597
+    //#obs# CodePen
+//- https://codepen.io/luizomf/full/KKNwMeG
+    //#obs# Github
+//-  https://github.com/luizomf/landing-page-cursojs
+    //#obs# Background SVG 
+//- https://www.svgbackgrounds.com/#confetti-doodles
+
+//_ Aula 598
     //#obs# Código da aula
 //- https://github.com/luizomf/landing-page-cursojs/commit/31010d033c30e279ef35f9e866f092716b0592aa
     //#obs# Validador de HTML
@@ -14,9 +22,9 @@
     //#obs# Accessinility check
 //- https://achecker.ca/checker/index.php
 
-//_
-    //#obs# 
-//-
+//_ Aula 599
+    //#obs# Código da aula
+//- https://github.com/luizomf/landing-page-cursojs/commit/13a32217f7b344efcca22ea89ff24501749b784c
 
 //_
     //#obs# 
